7b9f47cfe4adb9c7cec43eedb61a623e7abc8f9c
Magdalena <magdalenahyra@wp.pl>
2023-01-07 17:33:07 +0100

    Add calculateBill and calculateBillWithTip functions

---
+++ script.js
@@ -0,0 +1,38 @@
+let error = document.querySelector('.error');
+let costInfo = document.querySelector('.cost-info');
+let cost = document.querySelector('.cost');
+
+let price = document.querySelector('#price');
+let people = document.querySelector('#people');
+let tip = document.querySelector('#tip');
+
+let countBtn = document.querySelector('.count');
+
+countBtn.addEventListener('click', showBill);
+
+function showBill() {
+  if (price.value == '' || people.value == '') {
+    error.style.display = 'block';
+    costInfo.style.display = 'none';
+  } else if (tip.value == 0) {
+    calculateBill();
+  } else {
+    calculateBillWithTip();
+  }
+}
+
+function calculateBill() {
+  let bill = Number(price.value) / Number(people.value);
+  costInfo.style.display = 'block';
+  error.style.display = 'none';
+  cost.textContent = bill.toFixed(2);
+}
+
+function calculateBillWithTip() {
+  let BillwithTip =
+    (Number(price.value) + Number(price.value) * Number(tip.value)) /
+    Number(people.value);
+  costInfo.style.display = 'block';
+  error.style.display = 'none';
+  cost.textContent = BillwithTip.toFixed(2);
+}
